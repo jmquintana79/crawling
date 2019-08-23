514c2ec4006bb644e9a25dfecedc0d4e8b14a1c7
Juan Quintana <juan.quintana@infiniamobile.com>
2019-08-23 10:46:50 +0200

    include a bash script to download the spacy language models

---
+++ install_spacy_models.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# en
+/Users/admin/miniconda3/envs/crawling/bin/python3 -m spacy download en_core_web_md
+# es
+/Users/admin/miniconda3/envs/crawling/bin/python3 -m spacy download es_core_news_md
+# pt (only available sm size)
+#/Users/admin/miniconda3/envs/crawling/bin/python3 -m spacy download pt_core_news_sm
